39532da29a851fcec260f4adbacbd20ee57a2e77
Vidhan Shah <shahvidhan123@gmail.com>
2022-02-13 00:15:17 +0530

    Redeployed contract addresses :(

--- src/addresses.js
+++ src/addresses.js
@@ -2,13 +2,13 @@ const daiA = "0x001b3b4d0f3714ca98ba10f6042daebf0b1b7b6f";
 
 const oracleA = "0x83675c51089aEa68dbfc5c128695E258E9aB50b8";
 
-const mDaiA = "0x8E848185F93602622f1D7e6a73C3823E5Ea4062e";
+const mDaiA = "0x78D74787c20692E6a9BDC95FA8086D6DD2De53D2";
 
-const mMaticA = "0x10fb720ffAb1fa3a65d2a06c49bE966115256482";
+const mMaticA = "0x21aeAF58B8f08A7b4f374b26F4395B32F70eF5AE";
 
-const mltA = "0xffda80192a401ae9539B7141E508AbFE13830818";
+const mltA = "0x54091944E862ebDCe42f062E9EBfc8A3C4b10E55";
 
-const marketA = "0x9520cB9674a526b4182ef8E4Fde049566Bd305Cf";
+const marketA = "0xE5E9926D8915341D0aA43bb49aEd0CdE92226632";
 
 export { daiA, oracleA, mDaiA, mMaticA, mltA, marketA };
 
