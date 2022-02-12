8e1a3b5e30cacd0d5ba6531fe03d769340201e5a
Vidhan Shah <shahvidhan123@gmail.com>
2022-02-12 15:49:04 +0530

    Deployed contracts addresses

---
+++ src/addresses.js
@@ -0,0 +1,18 @@
+const daiA = "0x001b3b4d0f3714ca98ba10f6042daebf0b1b7b6f";
+
+const oracleA = "0x83675c51089aEa68dbfc5c128695E258E9aB50b8";
+
+const mDaiA = "0x8E848185F93602622f1D7e6a73C3823E5Ea4062e";
+
+const mMaticA = "0x10fb720ffAb1fa3a65d2a06c49bE966115256482";
+
+const mltA = "0xffda80192a401ae9539B7141E508AbFE13830818";
+
+const marketA = "0xef42b71187a5D7F359A0D227Efe3dc1FB35FA6F1";
+
+export { daiA, oracleA, mDaiA, mMaticA, mltA, marketA };
+
+/*
+1. Deploy oracle, mDai, mMatic, MLT, Market(with addresses)
+2. setAllowedContract to Market (for mDai, mMatic, MLT)
+*/
